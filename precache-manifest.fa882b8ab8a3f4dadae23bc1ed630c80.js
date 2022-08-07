cf45edf454edb2c980bb6c0a5a5c1bd25316a023
inhye kim <ggg6519@gmail.com>
2022-08-07 22:30:29 +1200

    Updates

---
+++ precache-manifest.fa882b8ab8a3f4dadae23bc1ed630c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "002e7d4363db764d1a3b",
+    "url": "/recipe-app/static/css/main.0e7ba54f.chunk.css"
+  },
+  {
+    "revision": "002e7d4363db764d1a3b",
+    "url": "/recipe-app/static/js/main.002e7d43.chunk.js"
+  },
+  {
+    "revision": "f01c6a14aefc7badd25b",
+    "url": "/recipe-app/static/css/1.073b2038.chunk.css"
+  },
+  {
+    "revision": "f01c6a14aefc7badd25b",
+    "url": "/recipe-app/static/js/1.f01c6a14.chunk.js"
+  },
+  {
+    "revision": "71cff4495fe45cdae67d",
+    "url": "/recipe-app/static/js/runtime~main.71cff449.js"
+  },
+  {
+    "revision": "18ca524fdc102e4ca160a46e7e27442b",
+    "url": "/recipe-app/index.html"
+  }
+];
